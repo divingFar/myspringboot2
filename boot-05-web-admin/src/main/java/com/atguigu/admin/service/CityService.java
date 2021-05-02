51663eeffbe2d2aba92b6d4edda8c3953e30c561
hezongkui <hezongkui@Gmail.com>
2021-05-02 11:23:30 +0800

    删除了自己写

---
+++ boot-05-web-admin/src/main/java/com/atguigu/admin/service/CityService.java
@@ -0,0 +1,12 @@
+package com.atguigu.admin.service;
+
+import com.atguigu.admin.bean.City;
+
+
+public interface CityService {
+
+     City getById(Long id);
+
+     void saveCity(City city);
+
+}
---
+++ boot-05-web-admin/src/main/java/com/atguigu/admin/service/UserService.java
@@ -0,0 +1,11 @@
+package com.atguigu.admin.service;
+
+import com.atguigu.admin.bean.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ *  Service 的CRUD也不用写了
+ */
+public interface UserService extends IService<User> {
+
+}
